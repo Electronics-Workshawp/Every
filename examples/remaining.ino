fd0bf994e80e99cf625c96f914c319974224c26d
Electronics-Workshawp <115026602+Electronics-Workshawp@users.noreply.github.com>
2023-01-25 02:26:12 -0500

    Create remaining.ino
    In this example, the Every object is created with an interval of 5 seconds, and a callback function for the first event is set. The update() method is called in the loop() function to check if the interval has passed. Inside the loop function, the remaining() method is used to check if the time remaining before the next trigger is less than or equal to half of the interval, if true, the secondEvent function is called.

---
+++ examples/remaining.ino
@@ -0,0 +1,24 @@
+#include <Every.h>
+
+Every everyFiveSeconds(5, "seconds");
+
+void firstEvent() {
+  // Do something every 5 seconds
+}
+
+void secondEvent() {
+  // Do something else when the first event is half way between triggers
+}
+
+void setup() {
+  everyFiveSeconds.setCallback(firstEvent);
+  // Initialize any necessary hardware or variables here
+}
+
+void loop() {
+  everyFiveSeconds.update();
+  if (everyFiveSeconds.remaining() <= (everyFiveSeconds.getInterval()/2)) {
+    // Trigger the second event when the first event is half way between triggers
+    secondEvent();
+  }
+}
