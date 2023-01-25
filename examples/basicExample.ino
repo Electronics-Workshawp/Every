3c6a7eb9524526523ed387b94efcf6537228538c
Electronics-Workshawp <115026602+Electronics-Workshawp@users.noreply.github.com>
2023-01-25 02:21:42 -0500

    Create basicExample.ino

---
+++ examples/basicExample.ino
@@ -0,0 +1,14 @@
+#include <Every.h>
+
+Every everyFiveSeconds(5, "seconds");
+
+void setup() {
+  // Initialize any necessary hardware or variables here
+}
+
+void loop() {
+  everyFiveSeconds.update();
+  if (everyFiveSeconds.triggered()) {
+    // Do something every 5 seconds
+  }
+}
