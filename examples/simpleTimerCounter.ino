7cf8170d809509516274406c83bbb9daa1f3310a
Electronics-Workshawp <115026602+Electronics-Workshawp@users.noreply.github.com>
2023-01-26 04:31:56 -0500

    Create simpleTimerCounter.ino

---
+++ examples/simpleTimerCounter.ino
@@ -0,0 +1,30 @@
+#include "Every.h"
+
+int seconds = 0;
+int minutes = 0;
+int hours = 0;
+
+Every oneSecond(1, Every::seconds);
+Every oneMinute(1, Every::minutes);
+Every oneHour(1, Every::hours);
+
+void updateSeconds() { seconds+=1; }
+void updateMinutes() { minutes+=1; }
+void updateHours() { hours+=1; }
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  oneSecond.setCallback(updateSeconds);
+  oneMinute.setCallback(updateMinutes);
+  oneHour.setCallback(updateHours);
+}
+
+void loop() {
+  oneSecond.update();
+  oneMinute.update();
+  oneHour.update();
+  String timerString = (String)seconds + ":" + (String)minutes + ":" + (String)hours;
+  Serial.println(timerString);
+}
