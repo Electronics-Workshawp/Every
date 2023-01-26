f698ca0a83e53829b543835ef979c0e0f6dee3c6
Electronics-Workshawp <115026602+Electronics-Workshawp@users.noreply.github.com>
2023-01-26 04:34:33 -0500

    added seconds and hours rollover

--- every.cpp
+++ every.cpp
@@ -1,129 +1,33 @@
-	#include "Arduino.h"
-	#include "Every.h"
-	
-	
+#include "Every.h"
 
-	Every::Every(uint32_t  interval, Unit unit) {
-	  _interval = interval;
-	  _previousTime = millis();
-	  _paused = false;
-	  _repeat = true;
-	  _callback = NULL;
-	  if (unit == milliseconds) {_interval *= 1;}
-	  else if (unit == seconds) {_interval *= 1000;}
-	  else if (unit == minutes) {_interval *= 1000*60;}
-	  else if (unit == hours) {_interval *= 1000*60*60;}
-	}
+int seconds = 0;
+int minutes = 0;
+int hours = 0;
+
+Every oneSecond(1, Every::seconds);
+Every oneMinute(1, Every::minutes);
+Every oneHour(1, Every::hours);
+
+void updateSeconds() { seconds+=1; }
+void updateMinutes() { minutes+=1; }
+void updateHours() { hours+=1; }
 
 
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  oneSecond.setCallback(updateSeconds);
+  oneMinute.setCallback(updateMinutes);
+  oneHour.setCallback(updateHours);
+}
 
-	bool Every::trigger() {
-	  if (_triggered) {
-		_triggered = false;
-		if(!_repeat) _paused = true;
-		if(_callback != NULL) _callback();
-		return true;
-	  }
-	  return false;
-	}
+void loop() {
+  oneSecond.update();
+  oneMinute.update();
+  oneHour.update();
+  String timerString = (String)seconds + ":" + (String)minutes + ":" + (String)hours;
 
-	void Every::update() {
-	  if(_paused) return;
-	  unsigned long currentTime = millis();
-	  if (currentTime - _previousTime >= _interval) {
-		_previousTime = currentTime;
-		_triggered = true;
-		if(_callback != NULL) _callback();
-	  }
-	  else if(currentTime < _previousTime) {
-		_previousTime = currentTime;
-	  }
-	  if(_triggered && !_repeat){
-		_paused = true;
-	  }
-	  _triggered = false;
-	}
-
-
-	uint32_t  Every::remaining(Unit unit) {
-	  uint32_t  currentTime = millis();
-	  if(_paused) return 0;
-	  if (_previousTime > currentTime) {
-		return 0;
-	  } else if (_previousTime + _interval > currentTime) {
-		uint32_t  remainingTime = _previousTime + _interval - currentTime;
-
-		if (unit == milliseconds) {return remainingTime / 1;}
-		else if (unit == seconds) {return remainingTime / 1000;}
-		else if (unit == minutes) {return remainingTime / (1000*60);}
-		else if (unit == hours) {return remainingTime / (1000*60*60);}
-	  } 
-	  else {
-		return 0;
-	  }
-	}
-
-
-
-	void Every::setInterval(float interval,Unit unit) {
-	  _interval = interval;
-	  
-	  if (unit == milliseconds){_interval *= 1;}
-	  else if (unit == seconds) {_interval *= 1000;}
-	  else if (unit == minutes) {_interval *= 1000*60;}
-	  else if (unit == hours) {_interval *= 1000*60*60;}
-	}
-
-
-	uint32_t   Every::getInterval(Unit unit) {
-	  uint32_t  interval = _interval;
-
-	  if (unit == milliseconds){return interval /= 1;}
-	  else if (unit == seconds){return interval /= 1000;}
-	  else if (unit == minutes){return interval /= 1000*60;}
-	  else if (unit == hours){return interval /= 1000*60*60;}
-	  else{
-		return 0;
-	  }
-	}
-
-
-	uint32_t  Every::percent() {
-	  uint32_t  currentTime = millis();
-	  if(_paused) return 0;
-	  if (_previousTime > currentTime) {
-		return 0;
-	  }
-	  else if (_previousTime + _interval > currentTime) {
-		uint32_t  timePassed = currentTime - _previousTime;
-		return (timePassed / (float)_interval) * 100;
-	  }
-	  else {
-		return 100;
-	  }
-	}
-
-	uint32_t  Every::mapValue(uint32_t  target) {
-	  uint32_t  output;
-	  uint32_t  currentTime = millis();
-	  if(_paused) return 0;
-	  if (_previousTime > currentTime) {
-		return 0;
-	  }
-	  else if (_previousTime + _interval > currentTime) {
-		uint32_t  timePassed = currentTime - _previousTime;
-		return (timePassed / (float)_interval) * target;
-	  }
-	  else {
-		return target;
-	  }
-	}
-
-
-	void Every::setRepeat(bool repeat) {
-	  _repeat = repeat;
-	}
-
-	void Every::setCallback(void (*callback)()) {
-	  _callback = callback;
-	}
+  if (seconds > 60){seconds = 0;)if (minutes > 60){minutes = 0;)
+ 
+  Serial.println(timerString);
+}
