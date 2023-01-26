268556365ccd57ac2ed9b3fdb0881fb578c4ef86
Electronics-Workshawp <115026602+Electronics-Workshawp@users.noreply.github.com>
2023-01-26 02:50:48 -0500

    Update every.cpp
    removed micros for now, updated with strcmp()

--- every.cpp
+++ every.cpp
@@ -1,144 +1,140 @@
-#include "Arduino.h"
-#include "Every.h"
+	#include "Arduino.h"
+	#include "Every.h"
+	
+	
 
-Every::Every(unsigned long interval, char* unit) {
-  _interval = interval;
-  _previousTime = micros();
-  _paused = false;
-  _repeat = true;
-  _callback = NULL;
-  if (unit == "microseconds") {
-    _interval *= 1;
-  }
-  if (unit == "milliseconds") {
-    _interval *= 1000;
-  }
-  if (unit == "seconds") {
-    _interval *= 1000000;
-  }
-  if (unit == "minutes") {
-    _interval *= 1000000*60;
-  }
-}
-
-bool Every::trigger() {
-  if (_triggered) {
-    _triggered = false;
-    if(!_repeat) _paused = true;
-    if(_callback != NULL) _callback();
-    return true;
-  }
-  return false;
-}
-
-void Every::update() {
-  if(_paused) return;
-  unsigned long currentTime = micros();
-  if (currentTime < _previousTime) {
-    _previousTime = currentTime;
-  }
-  else if (currentTime - _previousTime >= _interval) {
-    _previousTime = currentTime;
-    _triggered = true;
-  }
-}
-
-unsigned long Every::remaining(char* unit) {
-  unsigned long currentTime = micros();
-  if(_paused) return 0;
-  if (_previousTime > currentTime) {
-    return 0;
-  }
-  else if (_previousTime + _interval > currentTime) {
-    unsigned long remainingTime = _previousTime + _interval - currentTime;
-    if (unit == "microseconds") {
-      return remainingTime;
-    }
-    if (unit == "milliseconds") {
-      return remainingTime / 1000;
-    }
-    if (unit == "seconds") {
-      return remainingTime / 1000000;
-    }
-    if (unit == "minutes") {
-      return remainingTime / (1000000 * 60);
-    }
-  }
-  else {
-    return 0;
-  }
-}
+	Every::Every(unsigned long interval, char* unit) {
+	  _interval = interval;
+	  _previousTime = millis();
+	  _paused = false;
+	  _repeat = true;
+	  _callback = NULL;
+	  if (strcmp(unit, "milliseconds") == 0) {
+		_interval *= 1;
+	  } else if (strcmp(unit, "seconds") == 0) {
+		_interval *= 1000;
+	  } else if (strcmp(unit, "minutes") == 0) {
+		_interval *= 1000*60;
+	  }
+	}
 
 
-void Every::setInterval(float interval,char* unit) {
-  _interval = interval;
-  if (unit == "microseconds") {
-    _interval *= 1;
-  }
-  if (unit == "milliseconds") {
-    _interval *= 1000;
-  }
-  if (unit == "seconds") {
-    _interval *= 1000000;
-  }
-  if (unit == "minutes") {
-    _interval *= 1000000*60;
-  }
-}
 
-unsigned long  Every::getInterval(char* unit) {
-  unsigned long interval = _interval;	
-  if (unit == "microseconds") {
-    return interval /= 1;
-  }
-  if (unit == "milliseconds") {
-    return interval /= 1000;
-  }
-  if (unit == "seconds") {
-    return interval /= 1000000;
-  }
-  if (unit == "minutes") {
-    return interval /= 1000000*60;
-  }
-  
-}
+	bool Every::trigger() {
+	  if (_triggered) {
+		_triggered = false;
+		if(!_repeat) _paused = true;
+		if(_callback != NULL) _callback();
+		return true;
+	  }
+	  return false;
+	}
 
-unsigned long Every::percent() {
-  unsigned long currentTime = micros();
-  if(_paused) return 0;
-  if (_previousTime > currentTime) {
-    return 0;
-  }
-  else if (_previousTime + _interval > currentTime) {
-    unsigned long timePassed = currentTime - _previousTime;
-    return (timePassed / (float)_interval) * 100;
-  }
-  else {
-    return 100;
-  }
-}
+	void Every::update() {
+	  if(_paused) return;
+	  unsigned long currentTime = millis();
+	  if (currentTime < _previousTime) {
+		_previousTime = currentTime;
+	  }
+	  else if (currentTime - _previousTime >= _interval) {
+		_previousTime = currentTime;
+		_triggered = true;
+	  }
+	}
 
-unsigned long Every::mapValue(unsigned long target) {
-  unsigned long output;
-  unsigned long currentTime = micros();
-  if(_paused) return 0;
-  if (_previousTime > currentTime) {
-    return 0;
-  }
-  else if (_previousTime + _interval > currentTime) {
-    unsigned long timePassed = currentTime - _previousTime;
-    return (timePassed / (float)_interval) * target;
-  }
-  else {
-    return target;
-  }
-}
+	unsigned long Every::remaining(char* unit) {
+	  unsigned long currentTime = millis();
+	  if(_paused) return 0;
+	  if (_previousTime > currentTime) {
+		return 0;
+	  } else if (_previousTime + _interval > currentTime) {
+		unsigned long remainingTime = _previousTime + _interval - currentTime;
+
+		if (strcmp(unit, "milliseconds") == 0) {
+		  return remainingTime / 1;
+		}
+		else if (strcmp(unit, "seconds") == 0) {
+		  return remainingTime / 1000;
+		}
+		else if (strcmp(unit, "minutes") == 0) {
+		  return remainingTime / (1000 * 60);
+		}
+	  } else {
+		return 0;
+	  }
+	}
 
 
-void Every::setRepeat(bool repeat) {
-  _repeat = repeat;
-}
 
-void Every::setCallback(void (*callback)()) {
-  _callback = callback;
-}
+	void Every::setInterval(float interval,char* unit) {
+	  _interval = interval;
+	  
+	  if (strcmp(unit, "milliseconds") == 0) {
+		_interval *= 1;
+	  }
+	  else if (strcmp(unit, "seconds") == 0) {
+		_interval *= 1000;
+	  }
+	  else if (strcmp(unit, "minutes") == 0) {
+		_interval *= 1000*60;
+	  }
+	}
+
+
+	unsigned long  Every::getInterval(char* unit) {
+	  unsigned long interval = _interval;
+
+	  if (strcmp(unit, "milliseconds") == 0) {
+		return interval /= 1;
+	  }
+	  else if (strcmp(unit, "seconds") == 0) {
+		return interval /= 1000;
+	  }
+	  else if (strcmp(unit, "minutes") == 0) {
+		return interval /= 1000*60;
+	  }
+	  else{
+		return 0;
+	  }
+	}
+
+
+	unsigned long Every::percent() {
+	  unsigned long currentTime = millis();
+	  if(_paused) return 0;
+	  if (_previousTime > currentTime) {
+		return 0;
+	  }
+	  else if (_previousTime + _interval > currentTime) {
+		unsigned long timePassed = currentTime - _previousTime;
+		return (timePassed / (float)_interval) * 100;
+	  }
+	  else {
+		return 100;
+	  }
+	}
+
+	unsigned long Every::mapValue(unsigned long target) {
+	  unsigned long output;
+	  unsigned long currentTime = millis();
+	  if(_paused) return 0;
+	  if (_previousTime > currentTime) {
+		return 0;
+	  }
+	  else if (_previousTime + _interval > currentTime) {
+		unsigned long timePassed = currentTime - _previousTime;
+		return (timePassed / (float)_interval) * target;
+	  }
+	  else {
+		return target;
+	  }
+	}
+
+
+	void Every::setRepeat(bool repeat) {
+	  _repeat = repeat;
+	}
+
+	void Every::setCallback(void (*callback)()) {
+	  _callback = callback;
+	}
