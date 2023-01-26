a42a343c94aed11a0ba1d1425448f07b1ec9eb4c
Electronics-Workshawp <115026602+Electronics-Workshawp@users.noreply.github.com>
2023-01-26 03:04:54 -0500

    Create mapValue.ino

---
+++ examples/mapValue.ino
@@ -0,0 +1,19 @@
+#include <Every.h>
+
+Every timer(4, "seconds");
+
+void doSomething(){
+  Serial.println("test");
+}
+
+void setup() {
+  Serial.begin(115200);
+  timer.setCallback(doSomething);
+  // Initialize any necessary hardware or variables here
+}
+
+void loop() {
+  timer.update();
+  int counter = timer.mapValue(255)
+  Serial.println(counter);
+};
