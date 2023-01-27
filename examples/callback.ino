20ffcb2b65036249192d7618a1518549bd1b01b4
Electronics-Workshawp <115026602+Electronics-Workshawp@users.noreply.github.com>
2023-01-26 20:36:42 -0500

    Update callback.ino

--- examples/callback.ino
+++ examples/callback.ino
@@ -2,14 +2,14 @@
 
 Every timer(3000, Every::milliseconds);
 
-void blinkLED() {
+void myCallback() {
   Serial.println("CALLBACK TRIGGERED!!");
 }
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
-  timer.setCallback(blinkLED);
+  timer.setCallback(myCallback);
 }
 
 void loop() {
